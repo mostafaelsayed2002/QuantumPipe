cbb4b238b9104b72f9b4b9f654d7e5128ad8fafb
walidkh <walid.osama.khamees@gmail.com>
2023-12-01 18:34:33 +0200

    Fix: Data Memory Buses

--- DataMemory.vhd
+++ DataMemory.vhd
@@ -12,8 +12,9 @@ ENTITY DataMemory IS
         protect : IN STD_LOGIC;
         free : IN STD_LOGIC;
         addr : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        datain : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        dataout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+
+        datain : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        dataout : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
 END DataMemory;
 
@@ -25,27 +26,48 @@ ARCHITECTURE DataMemoryArch OF DataMemory IS
 
 BEGIN
     PROCESS (clk) IS
-        VARIABLE helper : STD_LOGIC_VECTOR(16 DOWNTO 0) := (OTHERS => '0');
+        VARIABLE helper33 : STD_LOGIC_VECTOR(33 DOWNTO 0) := (OTHERS => '0');
+        VARIABLE helper32 : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
+
     BEGIN
         IF rising_edge(clk) THEN
+            helper33(16 DOWNTO 0) := ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))));
+            helper33(33 DOWNTO 17) := ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))) + 1);
+
             IF protect = '1' THEN
-                helper := ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))));
-                helper(16) := '1';
-                ram(to_integer(UNSIGNED(addr(11 DOWNTO 0)))) <= helper;
+
+                helper33(16) := '1'
+                helper33(33) := '1';
+
+                ram(to_integer(UNSIGNED(addr(11 DOWNTO 0)))) <= helper33(16 DOWNTO 0);
+                ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))) + 1) <= helper33(33 DOWNTO 17);
+
             ELSIF free = '1' THEN
-                helper := ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))));
-                helper(16) := '0';
-                ram(to_integer(UNSIGNED(addr(11 DOWNTO 0)))) <= helper;
+
+                helper33(16) := '0'
+                helper33(33) := '0';
+
+                ram(to_integer(UNSIGNED(addr(11 DOWNTO 0)))) <= helper33(16 DOWNTO 0);
+                ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))) + 1) <= helper33(33 DOWNTO 17);
+
             ELSIF memwrite = '1' THEN
-                IF helper(16) = '0' THEN
-                    helper(15 DOWNTO 0) := datain;
-                    ram(to_integer(UNSIGNED(addr(11 DOWNTO 0)))) <= helper;
-                ELSE
-                    ram(to_integer(UNSIGNED(addr(11 DOWNTO 0)))) <= ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))));
+
+                IF helper33(16) = '0' AND helper33(33) = '0' THEN
+                    helper33(15 DOWNTO 0) := datain(15 DOWNTO 0);
+                    helper33(32 DOWNTO 17) := datain(31 DOWNTO 16);
+
+                    ram(to_integer(UNSIGNED(addr(11 DOWNTO 0)))) <= helper33(16 DOWNTO 0);
+                    ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))) + 1) <= helper33(33 DOWNTO 17);
                 END IF;
             ELSIF memread = '1' THEN
-                helper := ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))));
-                dataout <= helper(15 DOWNTO 0);
+                helper33(16 downto 0) := ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))));
+                helper33(33 downto 17) := ram(to_integer(UNSIGNED(addr(11 DOWNTO 0))) + 1);
+                
+                -- 32 to dataout
+                helper32(15 downto 0) := helper33(15 downto 0); 
+                helper32(31 downto 16) := helper33(32 downto 17); 
+
+                dataout <= helper32;
             END IF;
         END IF;
 
