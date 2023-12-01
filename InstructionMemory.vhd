3ef5d722ec00e05643a13dc5a85e0c42a1fc8648
walidkh <walid.osama.khamees@gmail.com>
2023-12-01 20:50:11 +0200

    chore: changed input name

--- InstructionMemory.vhd
+++ InstructionMemory.vhd
@@ -7,7 +7,7 @@ ENTITY InstructionMemory IS
     );
     PORT (
         clk : IN STD_LOGIC;
-        addr : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        pc : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         insout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
     );
 END InstructionMemory;
@@ -19,7 +19,7 @@ BEGIN
     PROCESS (clk)
     BEGIN
         IF rising_edge(clk) THEN
-            insout <= mem(to_integer(unsigned(addr(11 downto 0))));
+            insout <= mem(to_integer(unsigned(pc(11 downto 0))));
         END IF;
     END PROCESS;
 END InstructionMemoryArch; -- InstructionMemoryArch 
